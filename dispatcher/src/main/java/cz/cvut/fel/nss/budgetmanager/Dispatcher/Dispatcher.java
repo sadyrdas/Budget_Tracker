820eef8f14befd85a91b25b7b4d114d93b18b618
Arlan Nurkhozhin <dearmordor@gmail.com>
2023-04-25 00:01:13 +0200

    Configured Spring Boot apps and Docker environment

---
+++ dispatcher/src/main/java/cz/cvut/fel/nss/budgetmanager/Dispatcher/Dispatcher.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.nss.budgetmanager.Dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dispatcher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dispatcher.class, args);
+	}
+
+}
