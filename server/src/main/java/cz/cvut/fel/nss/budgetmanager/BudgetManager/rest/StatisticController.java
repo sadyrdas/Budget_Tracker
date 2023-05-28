6595a0f83883de99bf7fb74b3d2c95d57465db9c
lezhnevg <lezhnevg@fel.cvut.cz>
2023-05-28 21:49:37 +0200

    Added StatisticController

---
+++ server/src/main/java/cz/cvut/fel/nss/budgetmanager/BudgetManager/rest/StatisticController.java
@@ -0,0 +1,27 @@
+package cz.cvut.fel.nss.budgetmanager.BudgetManager.rest;
+
+import cz.cvut.fel.nss.budgetmanager.BudgetManager.model.TypeInterval;
+import cz.cvut.fel.nss.budgetmanager.BudgetManager.service.StatisticsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@RestController
+@RequestMapping("rest/statistics")
+public class StatisticController {
+
+    private final StatisticsService statisticsService;
+
+    public StatisticController(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+    @GetMapping(value="/generate")
+    public Map<String, BigDecimal> generateStatistics(@RequestParam TypeInterval intervalType) {
+        return statisticsService.generateStatistics(intervalType);
+    }
+}
